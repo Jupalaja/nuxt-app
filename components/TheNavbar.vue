6d1e845e791bf142c8496aacd13425617fb28ae9
Jupalaja <jupalaja@gmail.com>
2022-10-03 18:21:58 -0500

    Ready for deployment

---
+++ components/TheNavbar.vue
@@ -0,0 +1,43 @@
+<template>
+  <nav class="nav">
+    <div class="logo">
+      <nuxt-link :to="{name: 'index'}" class="logo text-lg">
+        Nuxt Fundamentals
+      </nuxt-link>
+      <span class="subheader">A vue school course</span>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "TheNavbar"
+}
+</script>
+
+<style scoped>
+nav {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 70px;
+  font-weight: lighter;
+  letter-spacing: 0.5px;
+}
+
+.nav .logo {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+.nav .logo .subheader {
+  opacity: 0.70;
+  font-size: 0.9rem;
+  color:white;
+}
+
+.nav .nav-content {
+  font-size: 1rem;
+}
+</style>
